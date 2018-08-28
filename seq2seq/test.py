09453aa1de0369b6a007b3e983c6e9bddf76d3a3
youichiro <cinnamon416@gmail.com>
2018-08-28 18:19:22 +0900

    fix

--- seq2seq/test.py
+++ seq2seq/test.py
@@ -3,7 +3,7 @@ import numpy
 from models import AttnBiSeq2Seq
 
 save_dir = 'tanaka_ja_en'
-model_file = 'e26-model.pkl'
+model_file = 'e30-model.pkl'
 vocabs_file = 'vocabs.pkl'
 hyper_file = 'hyperparameters.pkl'
 
@@ -14,6 +14,7 @@ with open(save_dir + '/' + hyper_file, 'rb') as f:
     hypers = pickle.load(f)
 
 src_w2id = vocabs['src_w2id']
+src_id2w = vocabs['src_id2w']
 tgt_w2id = vocabs['tgt_w2id']
 tgt_id2w = vocabs['tgt_id2w']
 src_vocab_size = len(src_w2id)
@@ -23,11 +24,13 @@ hidden_size = hypers['hidden_size']
 
 sent = '誰 が 一番 に 着 く か 私 に は 分か り ま せ ん 。'
 sent_ids = [src_w2id[word] for word in sent.split()]
+src = numpy.array([sent_ids])
 
 model = AttnBiSeq2Seq(src_vocab_size, tgt_vocab_size, wordvec_size, hidden_size)
 model.load_params(save_dir + '/' + model_file)
-predict = model.generate(numpy.array([sent_ids]), eos_id=tgt_w2id['<eos>'])
+predict = model.generate(src, eos_id=tgt_w2id['<eos>'])
 
 output = ' '.join([tgt_id2w[int(idx)] for idx in predict])
 print(sent)
 print(output)
+
